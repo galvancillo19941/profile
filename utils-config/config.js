ada16ca5e7edf7c53377767e9075c1222f58029b
Jesus David Galvan Gonzalez <galvancillo1994@gmail.com>
2021-05-08 14:33:39 -0500

    changes

--- utils-config/config.js
+++ utils-config/config.js
@@ -1,7 +1,7 @@
 // export const domain = process.env.DOMAIN || '.localhost'
 // export const apiURL = process.env.API_URL || 'http://localhost:3000/api/'
 
-export const domain = process.env.DOMAIN || 'https://perfil-next.herokuapp.com'
-export const apiURL = process.env.API_URL || 'https://perfil-next.herokuapp.com'
+export const domain = process.env.DOMAIN || 'https://perfil-next.herokuapp.com/'
+export const apiURL = process.env.API_URL || 'https://perfil-next.herokuapp.com/'
 
 
