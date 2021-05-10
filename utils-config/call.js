519300da2e40104f47a91b26dd21245e8be0d9ed
Jesus David Galvan Gonzalez <galvancillo1994@gmail.com>
2021-05-09 19:01:01 -0500

    changes

--- utils-config/call.js
+++ utils-config/call.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
-import { apiURL } from './config'
 
 function Call(url, method, data = null) {
     return axios({
         method: method,
-        url: `${apiURL}${url}`,
+        url: `https://perfil-develope.herokuapp.com/api/${url}`,
         data
     }).then((response) => {
         return response
