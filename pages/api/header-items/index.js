3bedc0a182ecafa988c48ecd537b018ccceb89b8
Jesus David Galvan Gonzalez <galvancillo1994@gmail.com>
2021-05-10 22:15:00 -0500

    changes

--- pages/api/header-items/index.js
+++ pages/api/header-items/index.js
@@ -1,23 +1,23 @@
-import { dataItems } from '../../../APIDATA/headerItems'
-
-export default (req, res) => {
-    const httpMethod = req.method;
-    const {slug} = req.body
-
-    switch (httpMethod) {
-        case 'GET':
-            res.status(200).json(dataItems)
-            break
-        case 'POST':
-            res.status(200).json({
-                slug: slug
-            })
-            break
-        default:
-            res.setHandlers('Allow', ['GET', 'POST'])
-            res.status(405).end(`Method ${httpMethod} Not Allowed`)
-
-    }
-
-    res.status(200).json(dataItems)
-}
+// import { dataItems } from '../../../APIDATA/headerItems'
+//
+// export default (req, res) => {
+//     const httpMethod = req.method;
+//     const {slug} = req.body
+//
+//     switch (httpMethod) {
+//         case 'GET':
+//             res.status(200).json(dataItems)
+//             break
+//         case 'POST':
+//             res.status(200).json({
+//                 slug: slug
+//             })
+//             break
+//         default:
+//             res.setHandlers('Allow', ['GET', 'POST'])
+//             res.status(405).end(`Method ${httpMethod} Not Allowed`)
+//
+//     }
+//
+//     res.status(200).json(dataItems)
+// }
