a6f92b8c63213e1a17f565ad1762c56d3a10dc1d
Jesus David Galvan Gonzalez <galvancillo1994@gmail.com>
2021-05-12 22:17:43 -0500

    changes

--- pages/api/my-proyects/index.js
+++ pages/api/my-proyects/index.js
@@ -1,23 +1,23 @@
-import { dataProjects } from '../../../APIDATA/dataProjects'
-
-export default (req, res) => {
-    const httpMethod = req.method;
-    const {slug} = req.body
-
-    switch (httpMethod) {
-        case 'GET':
-            res.status(200).json(dataProjects)
-            break
-        case 'POST':
-            res.status(200).json({
-                slug: slug
-            })
-            break
-        default:
-            res.setHandlers('Allow', ['GET', 'POST'])
-            res.status(405).end(`Method ${httpMethod} Not Allowed`)
-
-    }
-
-    res.status(200).json(dataProjects)
-}
+// import { dataProjects } from '../../../APIDATA/dataProjects'
+//
+// export default (req, res) => {
+//     const httpMethod = req.method;
+//     const {slug} = req.body
+//
+//     switch (httpMethod) {
+//         case 'GET':
+//             res.status(200).json(dataProjects)
+//             break
+//         case 'POST':
+//             res.status(200).json({
+//                 slug: slug
+//             })
+//             break
+//         default:
+//             res.setHandlers('Allow', ['GET', 'POST'])
+//             res.status(405).end(`Method ${httpMethod} Not Allowed`)
+//
+//     }
+//
+//     res.status(200).json(dataProjects)
+// }
